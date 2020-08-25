4af4aaf7c172844bffb03b8ab2bb05a9c509b492
“Shubham” <shubhampattar2@gmail.com>
2020-08-25 23:53:35 +0530

    ADDED Code To Calculate Wage Based On Condition.

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -4,38 +4,44 @@
 
 using namespace std;
 
-class EmployeeWageComputation {
-    int WAGE_PER_HOUR = 20;
-    int FULL_DAY_HOUR = 8;
-    int PART_TIME_HOUR = 4;
+class EmployeeWageComputation { 
     public:
         int dailyWage = 0;
         int getJobType();
-        int getWagePerHour();
-        int getFullDayHour();
-        int getPartTimeHour();
 };
 
 class Company {
     int WORK_DAYS_IN_MONTH = 20;
+    int WAGE_PER_HOUR = 20;
+    int FULL_DAY_HOUR = 8;
+    int PART_TIME_HOUR = 4;
+    int MAX_WORK_HOURS_IN_MONTH = 100;
     
     public:
-        int getWorkDays();
+    int getWagePerHour();
+    int getFullDayHour();
+    int getPartTimeHour();
+    int getWorkDays();
+    int getMaxWorkHours();
 };
 
 int Company::getWorkDays() {
     return this->WORK_DAYS_IN_MONTH;
 }
 
-int EmployeeWageComputation::getWagePerHour() {
+int Company::getMaxWorkHours() {
+    return this->MAX_WORK_HOURS_IN_MONTH;
+}
+
+int Company::getWagePerHour() {
     return this->WAGE_PER_HOUR;
 }
 
-int EmployeeWageComputation::getFullDayHour() {
+int Company::getFullDayHour() {
     return this->FULL_DAY_HOUR;
 }
 
-int EmployeeWageComputation::getPartTimeHour() {
+int Company::getPartTimeHour() {
     return this->PART_TIME_HOUR;
 }
 
@@ -50,33 +56,33 @@ int main() {
     Company company;
     srand(time(0));
     int monthlyWage = 0;
-    int workHrs = 0, totalWorkHrs = 0;
+    int workHrs = 0, totalWorkHrs = 0, totalWorkDays = 1;
 
     cout << "Welcome To Employee Wage Computation" << endl << endl;
 
-    for(int day = 1; day <= company.getWorkDays(); day++) {
-
+    while(totalWorkHrs < company.getMaxWorkHours() && totalWorkDays < company.getWorkDays()) {
+        totalWorkDays++;
         int jobType = employee.getJobType();
 
         switch(jobType) {
             case 1:
-                workHrs = employee.getFullDayHour();
-                cout << "Day" << day << " WorkHr = " << workHrs << endl;
+                workHrs = company.getFullDayHour();
+                cout << "Day" << totalWorkDays << " WorkHr = " << workHrs << endl;
                 break;
             case 2:
-                workHrs = employee.getPartTimeHour();
-                cout << "Day" << day << " WorkHr = " << workHrs << endl;
+                workHrs = company.getPartTimeHour();
+                cout << "Day" << totalWorkDays << " WorkHr = " << workHrs << endl;
                 break;
             default:
                 workHrs = 0;
-                cout << "Day" << day << " WorkHr = " << workHrs << endl;
+                cout << "Day" << totalWorkDays << " WorkHr = " << workHrs << endl;
                 break;
         }
        totalWorkHrs += workHrs;
     }
 
     cout << "Total Hours Worked: " << totalWorkHrs << endl;
-    monthlyWage = totalWorkHrs * employee.getWagePerHour();
+    monthlyWage = totalWorkHrs * company.getWagePerHour();
     cout << "Monthly Wage = " << monthlyWage << endl;
     return 0;
 }
