1e20f03580c3a518c448f874e3a70043103f1652
“Shubham” <shubhampattar2@gmail.com>
2020-08-25 21:30:47 +0530

    ADDED Code To Calculate Wage For Part Time And Full Time Employee.

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -6,11 +6,13 @@ using namespace std;
 class EmployeeWageComputation {
     int WAGE_PER_HOUR = 20;
     int FULL_DAY_HOUR = 8;
+    int PART_TIME_HOUR = 4;
     public:
         int dailyWage = 0;
-        int checkPresent();
+        int getJobType();
         int getWagePerHour();
         int getFullDayHour();
+        int getPartTimeHour();
 };
 
 int EmployeeWageComputation::getWagePerHour() {
@@ -21,25 +23,36 @@ int EmployeeWageComputation::getFullDayHour() {
     return this->FULL_DAY_HOUR;
 }
 
-int EmployeeWageComputation::checkPresent() {
+int EmployeeWageComputation::getPartTimeHour() {
+    return this->PART_TIME_HOUR;
+}
+
+int EmployeeWageComputation::getJobType() {
     srand(time(0));
-    return (rand() % 2);
+    return (rand() % 3) + 1;
 }
 
 int main() {
     
     EmployeeWageComputation employee;
-    int attendence = employee.checkPresent();
+    int jobType = employee.getJobType();
 
     cout << "Welcome To Employee Wage Computation" << endl << endl;
 
-    if(attendence == 1) {
-        cout << "Employee is Present" << endl;
-        employee.dailyWage = employee.getWagePerHour() * employee.getFullDayHour();
-        cout << "Wage = " << employee.dailyWage;
+    switch(jobType) {
+        case 1:
+            cout << "Employee is Full Time\n";
+            employee.dailyWage = employee.getWagePerHour() * employee.getFullDayHour();
+            cout << "Wage = " << employee.dailyWage;
+            break;
+        case 2:
+            cout << "Employee is Part Time\n";
+            employee.dailyWage = employee.getWagePerHour() * employee.getPartTimeHour();
+            cout << "Wage = " << employee.dailyWage;
+            break;
+        default:
+            cout << "Employee is Absent\n";
     }
-    else {
-        cout << "Employee is Absent" << endl;
-    }    
+
     return 0;
 }
