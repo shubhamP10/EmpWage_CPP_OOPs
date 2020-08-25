674904ea82dc30ee99af686d91ba4d50d10e0241
“Shubham” <shubhampattar2@gmail.com>
2020-08-25 20:52:34 +0530

    ADDED Code For Employee Attendence.

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+class EmployeeWageComputation {
+    public:
+        int checkPresent();
+};
+
+int EmployeeWageComputation::checkPresent() {
+    srand(time(0));
+    return (rand() % 2);
+}
+
 int main() {
+    
+    EmployeeWageComputation employee;
+    int attendence = employee.checkPresent();
 
     cout << "Welcome To Employee Wage Computation" << endl;
+
+    if(attendence == 1) {
+        cout << "Employee is Present" << endl;
+    }
+    else {
+        cout << "Employee is Absent" << endl;
+    }    
     return 0;
 }
