30c4ea63fb10f7e9b3935f6203379d74d55c86aa
“Shubham” <shubhampattar2@gmail.com>
2020-08-25 21:02:11 +0530

    ADDED Code To Calculate Employee Daily Wage.

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -4,9 +4,22 @@
 using namespace std;
 
 class EmployeeWageComputation {
+    int WAGE_PER_HOUR = 20;
+    int FULL_DAY_HOUR = 8;
     public:
+        int dailyWage = 0;
         int checkPresent();
+        int getWagePerHour();
+        int getFullDayHour();
 };
+
+int EmployeeWageComputation::getWagePerHour() {
+    return this->WAGE_PER_HOUR;
+}
+
+int EmployeeWageComputation::getFullDayHour() {
+    return this->FULL_DAY_HOUR;
+}
 
 int EmployeeWageComputation::checkPresent() {
     srand(time(0));
@@ -18,10 +31,12 @@ int main() {
     EmployeeWageComputation employee;
     int attendence = employee.checkPresent();
 
-    cout << "Welcome To Employee Wage Computation" << endl;
+    cout << "Welcome To Employee Wage Computation" << endl << endl;
 
     if(attendence == 1) {
         cout << "Employee is Present" << endl;
+        employee.dailyWage = employee.getWagePerHour() * employee.getFullDayHour();
+        cout << "Wage = " << employee.dailyWage;
     }
     else {
         cout << "Employee is Absent" << endl;
